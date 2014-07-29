b028cbf1ade51cabc8b500c19c0309e7b910a35d
zuijiawoniu <panhuahuang@gmail.com>
2014-07-29 21:21:45 +0800

    Update google.py
    using 'https' instead of 'http' in Geocoding API requests.

--- geocoders/google.py
+++ geocoders/google.py
@@ -3,11 +3,15 @@ from utils import simplejson, geocoder_factory
 
 # https://developers.google.com/maps/documentation/geocoding/
 
+#update:google apply no policy in using geocoding api, including 'https' instead of 'http',2500 requests/day.
+#you can apply google api key in website https://console.developers.google.com after  creating project, then turn on the service "Geocoding API" and create new key for browser application in "Credentials".
+#you can then test your google api key in browser like this:https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=new york&key=AIzaSyCTiFJreqzvYieWdH555m--bVnO70qeJ4E
 
 def geocode(q, api_key=None):
     json = simplejson.load(urllib.urlopen(
-        'http://maps.googleapis.com/maps/api/geocode/json?' + urllib.urlencode({
+        'https://maps.googleapis.com/maps/api/geocode/json?' + urllib.urlencode({
             'address': q,
+            'key':api_key,
             'sensor': 'false',
         })
     ))
